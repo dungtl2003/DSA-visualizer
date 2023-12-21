46e0fd6f29373e89f2ae1fcbaf39ad2aa83d5225
huynguyengia <huynguyengia1203@gmail.com>
2023-12-21 09:31:09 +0700

    chore: bug run all instructions at the same time

--- src/client/amination/sort/mergesort.js
+++ src/client/amination/sort/mergesort.js
@@ -1,26 +1,83 @@
-const relocate = function (instructions) {
-    let columns = document.getElementsByClassName(
-        "body__main-content__display__col"
-    );
+const sleepTime = 3000;
+const animateDuration = 1500;
 
-    // compare columns
+const columnContainer = document.getElementById(
+    "body__main-content__display__frame"
+);
 
-    // combine into sorted columns
-    for (let i = 0; i < columns.length; i++) {
-        if (i === start) {
-            let column = columns[i];
-
-            column.setAttribute("data-percentage");
-            reDrawColumn(column);
-        }
-    }
-};
-
-const reDrawColumn = function (columnElement) {
-    columnElement.animate(
+const drawColumn = function (columnElement) {
+    $(columnElement).animate(
         {
-            height: columnElement.data("percentage") + "%",
+            height: columnElement.getAttribute("data-percentage") + "%",
         },
         animateDuration
     );
 };
+
+const paintColumn = function (column, color, time) {
+    const oldColor = column.getAttribute("background-color");
+    column.style.background = color;
+    setTimeout(() => {
+        column.style.background = oldColor;
+    }, time);
+};
+
+const sleep = async () => {
+    setTimeout(() => {}, sleepTime);
+};
+
+const compare = async function (instruction, columns) {
+    if (instruction.p1 !== -1) {
+        paintColumn(columns[instruction.p1].childNodes[0], "red", sleepTime);
+    }
+    if (instruction.p2 !== -1) {
+        paintColumn(columns[instruction.p2].childNodes[0], "red", sleepTime);
+    }
+};
+
+const divide = async function (instruction, columns) {
+    // columns[instruction.start].setAttribute("background-color", "yellow");
+    // columns[instruction.mid].setAttribute("background-color", "yellow");
+    // columns[instruction.mid + 1].setAttribute("background-color", "blue");
+    // columns[instruction.end].setAttribute("background-color", "blue");
+    // sleep();
+};
+
+const merge = async function (instruction, columns) {
+    let dataIndex = 0;
+    for (let i = instruction.start; i <= instruction.end; i++) {
+        columns[i].setAttribute(
+            "data-percentage",
+            instruction.values[dataIndex++]
+        );
+        drawColumn(columns[index]);
+    }
+};
+
+const executeAsync = async (instruction) => {
+    let columns = columnContainer.children;
+    switch (instruction.type) {
+        case "divide":
+            await divide(instruction, columns);
+            break;
+        case "compare":
+            await compare(instruction, columns);
+            break;
+        case "merge":
+            console.log("merge");
+            await merge(instruction, columns);
+            break;
+    }
+};
+
+const runSortingAnimation = async function (instructions) {
+    const date = new Date();
+    const start = date.getSeconds();
+    for (const instruction of instructions) {
+        await executeAsync(instruction);
+        await sleep();
+    }
+    console.log("Execute time", date.getSeconds() - start);
+};
+
+export default runSortingAnimation;
--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,28 @@
 "use strict";
 
 import "./common/setDefault.js";
-import { drawColumns } from "./common/utils/array.js";
+import {drawColumns} from "./common/utils/array.js";
+import mergesort from "./components/sort/mergesort.js";
+import runSortingAnimation from "./amination/sort/mergesort.js";
 
 const slider = document.getElementById("body__sidebar__slider");
 const colNumberDisplay = document.getElementById("body__sidebar__col-num");
+const columnContainer = document.getElementById(
+    "body__main-content__display__frame"
+);
+const btn = document.getElementById("body__sidebar__btn-item--solve");
 
 slider.onchange = function () {
     colNumberDisplay.innerHTML = this.value;
     drawColumns(Number(this.value));
 };
+
+btn.addEventListener("click", function (e) {
+    const values = [];
+    const children = columnContainer.children;
+    for (const child of children) {
+        values.push(Number(child.getAttribute("data-percentage")));
+    }
+    const instructions = mergesort(values);
+    runSortingAnimation(instructions);
+});
