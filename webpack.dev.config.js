2726811713676bd86ada62a183016e2c9f829da6
ilikeblue <luudung0806@gmail.com>
2023-12-23 21:56:08 +0700

    feat: add webpack production config

---
+++ webpack.dev.config.js
@@ -0,0 +1,44 @@
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const config = {
+    mode: "development",
+    devServer: {
+        port: 9000,
+        static: {
+            directory: __dirname + "/dist",
+        },
+        devMiddleware: {
+            index: "index.html",
+            writeToDisk: true,
+        },
+    },
+    entry: {
+        app: "./client/index.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
+        clean: true,
+    },
+    module: {
+        rules: [
+            {test: /\.css$/, use: ["style-loader", "css-loader"]},
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "client/index.html",
+        }),
+    ],
+};
+
+export default config;
---
+++ webpack.prod.config.js
@@ -0,0 +1,38 @@
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const config = {
+    mode: "production",
+    entry: {
+        app: "./client/index.js",
+    },
+    output: {
+        filename: "[name].[contenthash].bundle.js",
+        path: __dirname + "/dist",
+        clean: true,
+    },
+    module: {
+        rules: [
+            {test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"]},
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "client/index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "styles.[contenthash].css",
+        }),
+    ],
+};
+
+export default config;
