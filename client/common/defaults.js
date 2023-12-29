b9d18c57c0276ae6434f2cd8654296a69fe21192
huynguyengia <huynguyengia1203@gmail.com>
2023-12-30 00:01:09 +0700

    feat: add logo

--- client/common/defaults.js
+++ client/common/defaults.js
@@ -8,6 +8,7 @@ import "../asset/css/base.css";
 import "../asset/css/main.css";
 import "../asset/css/support.css";
 import "../asset/css/reponsive.css";
+import image from "../asset/img/logo.png";
 const sliderDefaultValue = 5;
 const sliderMaxCols = 25;
 const sliderMinCols = 5;
@@ -16,6 +17,9 @@ const minSpeed = 20000;
 
 const colsNumberDisplay = document.getElementById("body__sidebar__col-num");
 const slider = document.getElementById("body__sidebar__slider");
+const iconContainer = document.getElementById(
+    "header__navbar__left-section__logo-container"
+);
 
 const getComponents = async function () {
     const {drawColumns} = await import("./column.js");
@@ -42,6 +46,11 @@ const init = function (values) {
     colsNumberDisplay.setAttribute("value", sliderDefaultValue);
 
     drawColumns(sliderDefaultValue);
+    const icon = document.createElement("img");
+    icon.setAttribute("src", image);
+    icon.setAttribute("class", "header__navbar__left-section__logo");
+    icon.setAttribute("alt", "logo.img");
+    iconContainer.appendChild(icon);
 };
 
 export {sliderDefaultValue, sliderMinCols, sliderMaxCols, minSpeed, maxSpeed};
